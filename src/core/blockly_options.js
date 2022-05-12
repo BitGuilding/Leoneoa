28f284c8590ae662ce5c04b75a4b88bf2e8d98b0
BitGuilding <94827355+BitGuilding@users.noreply.github.com>
2022-05-12 14:59:23 +0800

    Create blockly_options.js

---
+++ src/core/blockly_options.js
@@ -0,0 +1,22 @@
+/**
+ * @fileoverview Object that defines user-specified options for the workspace.
+ */
+'use strict';
+
+/**
+ * Object that defines user-specified options for the workspace.
+ * @namespace Blockly.BlocklyOptions
+ */
+goog.module('Blockly.BlocklyOptions');
+
+
+/**
+ * Blockly options.
+ * This interface is further described in
+ * `typings/parts/blockly-interfaces.d.ts`.
+ * @interface
+ * @alias Blockly.BlocklyOptions
+ */
+const BlocklyOptions = function() {};
+
+exports.BlocklyOptions = BlocklyOptions;
