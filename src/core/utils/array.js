3c80b5ec74925c59b19e803d69e9e93ee5b57aa7
BitGuilding <94827355+BitGuilding@users.noreply.github.com>
2022-05-21 15:58:41 +0800

    Create array.js

---
+++ src/core/utils/array.js
@@ -0,0 +1,28 @@
+/**
+ * @fileoverview Utility methods related to arrays.
+ */
+'use strict';
+
+/**
+ * @namespace Blockly.utils.array
+ */
+goog.module('Blockly.utils.array');
+
+
+/**
+ * Removes the first occurrence of a particular value from an array.
+ * @param {!Array} arr Array from which to remove value.
+ * @param {*} value Value to remove.
+ * @return {boolean} True if an element was removed.
+ * @alias Blockly.array.removeElem
+ * @package
+ */
+const removeElem = function(arr, value) {
+  const i = arr.indexOf(value);
+  if (i === -1) {
+    return false;
+  }
+  arr.splice(i, 1);
+  return true;
+};
+exports.removeElem = removeElem;
